9e228dfe1f9a66e5e261fd789bec71832038ec63
Jasper Ginn <jasperginn@gmail.com>
2024-01-26 18:36:34 +0100

    build: update start time

--- dags/luchtmeetnet_ingestion/src/luchtmeetnet_ingestion/partitions.py
+++ dags/luchtmeetnet_ingestion/src/luchtmeetnet_ingestion/partitions.py
@@ -15,7 +15,7 @@ with (impresources.files(static) / "stations.json").open("r") as f:
     stations = [sd["number"] for sd in json.load(f)]
 
 daily_partition = DailyPartitionsDefinition(
-    start_date=datetime.datetime(2024, 1, 1),
+    start_date=datetime.datetime(2024, 1, 24),
     end_offset=0,
     timezone="Europe/Amsterdam",
     fmt="%Y-%m-%d",
