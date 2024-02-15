1fb2cb254644e542b409fb63b47aaa7ebcc65b4b
Jasper Ginn <jasperginn@gmail.com>
2024-02-15 13:20:30 +0100

    chore: uncomments and fix version loading

--- shared/dagster_utils/src/dagster_utils/__init__.py
+++ shared/dagster_utils/src/dagster_utils/__init__.py
@@ -1,6 +1,6 @@
 from importlib import metadata
 
 try:
-    __version__ = metadata.version("delt")
+    __version__ = metadata.version("dagster_utils")
 except metadata.PackageNotFoundError:
     __version__ = "0.0.0"
