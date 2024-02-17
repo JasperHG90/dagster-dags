19631011a1d21a76db81c80ecd23d1e0175d8487
Jasper Ginn <jasperginn@gmail.com>
2024-02-17 15:56:17 +0100

    feat: add utility functions

--- shared/dagster_scripts/src/utils.py
+++ shared/dagster_scripts/src/utils.py
@@ -1,8 +1,13 @@
+import functools
+import logging
 import pathlib as plb
 import typing
 
 import yaml
+from dagster_graphql.client import DagsterGraphQLClient, DagsterGraphQLClientError
 from pydantic import BaseModel
+
+logger = logging.getLogger("dagster_scripts.utils")
 
 
 def load_config(path: typing.Union[str, plb.Path], config_cls: BaseModel) -> BaseModel:
@@ -14,3 +19,37 @@ def load_config(path: typing.Union[str, plb.Path], config_cls: BaseModel) -> Bas
         cnf_raw = yaml.safe_load(inFile)
         cnf_out = config_cls(**cnf_raw)
         return cnf_out
+
+
+def graphql_client(host: str = "localhost", port: int = 3000):
+    """Decorator to provide the Dagster GraphQL client to a function"""
+
+    def decorator(f):
+        functools.wraps(f)
+
+        def wrapper(*args, **kwargs):
+            client = DagsterGraphQLClient(host, port_number=port)
+            return f(*args, client=client, **kwargs)
+
+        return wrapper
+
+    return decorator
+
+
+def request_job_run(
+    tags: typing.Dict[str, str],
+    job_name: str,
+    repository_name: str,
+    client: typing.Optional[DagsterGraphQLClient] = None,
+):
+    """Request a new job run from the Dagster GraphQL API"""
+    if client is None:
+        raise ValueError("No client provided")
+    _client: DagsterGraphQLClient = client
+    try:
+        new_run_id: str = _client.submit_job_execution(
+            job_name=job_name, repository_name=repository_name, run_config={}, tags=tags
+        )
+        return new_run_id
+    except DagsterGraphQLClientError as e:
+        logger.error(f"Error submitting job execution: {e}")
