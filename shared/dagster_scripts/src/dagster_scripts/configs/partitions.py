dca28bff139f7b9d1c5c20788a0aa32daf7e3924
Jasper Ginn <jasperginn@gmail.com>
2024-02-22 08:21:49 +0100

    chore: make partition configs consistent

--- shared/dagster_scripts/src/dagster_scripts/configs/partitions.py
+++ shared/dagster_scripts/src/dagster_scripts/configs/partitions.py
@@ -1,5 +1,5 @@
-import typing
 import datetime as dt
+import typing
 from abc import ABC, abstractmethod
 
 import pendulum
@@ -8,25 +8,30 @@ from pydantic import BaseModel
 
 class PartitionConfig(BaseModel, ABC):
     name: str
+    values: typing.Any
 
     @abstractmethod
     def config_dict(self):
         pass
 
 
+class DateRangeConfig(BaseModel):
+    start_date: dt.date
+    end_date: dt.date
+
+
 class DatePartitionConfig(PartitionConfig):
-	start_date: dt.date
-	end_date: dt.date
+    values: DateRangeConfig
 
-	def _get_range(self):
-		return pendulum.period(self.start_date, self.end_date).range('days')
+    def _get_range(self):
+        return pendulum.period(self.values.start_date, self.values.end_date).range("days")
 
-	def config_dict(self):
-		return [{f"dagster/partition/{self.name}": d.to_date_string()} for d in self._get_range()]
+    def config_dict(self):
+        return [{f"dagster/partition/{self.name}": d.to_date_string()} for d in self._get_range()]
 
 
 class StaticPartitionConfig(PartitionConfig):
-	values: typing.List[str]
+    values: typing.List[str]
 
-	def config_dict(self):
-		return [{f"dagster/partition/{self.name}": v} for v in self.values]
+    def config_dict(self):
+        return [{f"dagster/partition/{self.name}": v} for v in self.values]
