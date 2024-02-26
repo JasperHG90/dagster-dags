15a30f5fcdf68f42e107900c0c0d377f1fa880bc
Jasper Ginn <jasperginn@gmail.com>
2024-02-26 17:59:42 +0100

    chore: set ingestionn job back to 3AM

--- dags/luchtmeetnet_ingestion/src/luchtmeetnet_ingestion/schedules.py
+++ dags/luchtmeetnet_ingestion/src/luchtmeetnet_ingestion/schedules.py
@@ -1,6 +1,4 @@
 from dagster import build_schedule_from_partitioned_job
 from luchtmeetnet_ingestion.jobs import ingestion_job
 
-daily_schedule = build_schedule_from_partitioned_job(
-    job=ingestion_job, hour_of_day=13, minute_of_hour=42
-)
+daily_schedule = build_schedule_from_partitioned_job(job=ingestion_job, hour_of_day=3)
