c76fc39d09db8125bf819ac1794eaf9a18f64de2
Jasper Ginn <jasperginn@gmail.com>
2024-03-22 16:02:33 +0100

    chore: re-arrange util

--- dags/luchtmeetnet_ingestion/src/luchtmeetnet_ingestion/assets/__init__.py
+++ dags/luchtmeetnet_ingestion/src/luchtmeetnet_ingestion/assets/__init__.py
@@ -9,7 +9,6 @@ from dagster import (
     AutoMaterializeRule,
     Backoff,
     DataVersion,
-    Failure,
     FreshnessPolicy,
     Jitter,
     MultiToSingleDimensionPartitionMapping,
@@ -17,6 +16,7 @@ from dagster import (
     RetryPolicy,
     asset,
 )
+from luchtmeetnet_ingestion.assets.utils import get_air_quality_data_for_partition_key
 from luchtmeetnet_ingestion.IO.resources import LuchtMeetNetResource
 from luchtmeetnet_ingestion.partitions import (
     daily_partition,
@@ -24,34 +24,6 @@ from luchtmeetnet_ingestion.partitions import (
     stations_partition,
 )
 from pandas.util import hash_pandas_object
-from requests import HTTPError
-
-
-def get_air_quality_data_for_partition_key(
-    partition_key: str, context: AssetExecutionContext, luchtmeetnet_api: LuchtMeetNetResource
-) -> pd.DataFrame:
-    date, station = partition_key.split("|")
-    context.log.debug(date)
-    context.log.debug(f"Fetching data for {date}")
-    start, end = f"{date}T00:00:00", f"{date}T23:59:59"
-    rp = {"start": start, "end": end, "station_number": station}
-    try:
-        df = pd.DataFrame(
-            luchtmeetnet_api.request("measurements", context=context, request_params=rp)
-        )
-    # We don't want to keep retrying for a station that is raising code 500
-    except HTTPError as e:
-        if e.response.status_code == 500:
-            raise Failure(
-                description=f"Received HTTP status code 500 Failed to fetch data for {date} and station {station}. Skipping retries ...",
-                allow_retries=False,
-            ) from e
-        else:
-            raise e
-    df["station_number"] = station
-    df["start"] = start
-    df["end"] = end
-    return df
 
 
 @asset(
@@ -117,9 +89,7 @@ def air_quality_data(
     code_version="v1",
     group_name="measurements",
 )
-def daily_air_quality_data(
-    context: AssetExecutionContext, ingested_data: pd.DataFrame
-) -> pd.DataFrame:
+def daily_air_quality_data(ingested_data: pd.DataFrame) -> pd.DataFrame:
     return ingested_data
 
 
@@ -162,7 +132,5 @@ def station_names(
     ),
     group_name="stations",
 )
-def air_quality_station_names(
-    context: AssetExecutionContext, station_names: pd.DataFrame
-) -> pd.DataFrame:
+def air_quality_station_names(station_names: pd.DataFrame) -> pd.DataFrame:
     return station_names
